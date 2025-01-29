d609dbfb10d093942cceb1f8e68d90d66b8368eb
idris <kulubiidris@gmail.com>
2025-01-29 15:21:10 +0300

    new enhancement

---
+++ drizzle/0003_chunky_enchantress.sql
@@ -0,0 +1,26 @@
+ALTER TABLE "profiles" ALTER COLUMN "bio" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "bio" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "age" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "age" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "gender" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "gender" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "interests" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "interests" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "photos" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "photos" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "looking_for" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "looking_for" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "course" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "course" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "year_of_study" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "year_of_study" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "instagram" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "spotify" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "snapchat" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "profile_photo" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "profile_photo" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "phone_number" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "first_name" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "first_name" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "last_name" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "profiles" ALTER COLUMN "last_name" DROP NOT NULL;
